17f2b137f916023c27d03b49a2803d29c472105a
Ali Burak KAYA <aliburakkayas@hotmail.com>
2024-06-28 19:50:15 +0300

    update

--- components/common/pagination.jsx
+++ components/common/pagination.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import Link from 'next/link';
+import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
+import styles from '@/styles/components/common/pagination.module.scss';
 
-export const Pagination = () => {
-  return (
-    <div>Pagination</div>
-  )
-}
+export const Pagination = ({ baseUrl, currentPage, size, totalPages }) => {
+    
+    const previousPage = currentPage === 1 ? currentPage : currentPage - 1;
+    const nextPage = currentPage === totalPages ? currentPage : currentPage + 1;
+
+    return (
+        <div className={styles.pagination}>
+            {/* FIRST PAGE */}
+            <Link
+                href={`${baseUrl}?size=${size}&page=1`}
+                title="Go To First Page"
+            >
+                <IoChevronBackOutline size={30} />
+                <IoChevronBackOutline size={30} />
+            </Link>
+            {/* PREVIOUS PAGE */}
+            <Link
+                href={`${baseUrl}?size=${size}&page=${previousPage}`}
+                title="Go To Previous Page"
+            >
+                <IoChevronBackOutline size={30} />
+            </Link>
+
+            {/* CURRENT PAGE */}
+            <div className={styles.pages}></div>
+
+            {/* NEXT PAGE */}
+            <Link
+                href={`${baseUrl}?size=${size}&page=${nextPage}`}
+                title="Go To Next Page"
+            >
+                <IoChevronForwardOutline size={30} />
+            </Link>
+            {/* LAST PAGE */}
+            <Link
+                href={`${baseUrl}?size=${size}&page=${totalPages}`}
+                title="Go To Last Page"
+            >
+                <IoChevronForwardOutline size={30} />
+                <IoChevronForwardOutline size={30} />
+            </Link>
+        </div>
+    );
+};
--- components/dashboard/admin-list.jsx
+++ components/dashboard/admin-list.jsx
@@ -1,12 +1,12 @@
 import styles from '@/styles/list.module.scss';
 import { NoDataAvailable } from '@/components/common/no-data-available';
-import { AdminCard } from '@/components/dashboard/cards/admin-card';
+import { AdminCard } from '@/components/cards/admin-card';
 import { getAdminsByPage } from '@/actions/admin/get-admins-by-page.action';
 import { calculateOrderNumber } from '@/utils/functions/calculate-order-number';
 import { Pagination } from '@/components/common/pagination';
 
 export const AdminList = async ({ page, size, sort, type }) => {
-    const data = await getAdminsByPage({ page: page-1, size, sort, type });
+    const data = await getAdminsByPage({ page: page - 1, size, sort, type });
 
     const isDataAvailable =
         data &&
@@ -19,14 +19,27 @@ export const AdminList = async ({ page, size, sort, type }) => {
             <div className={styles.cardsContainer}>
                 {isDataAvailable ? (
                     data.content.map((item, index) => (
-                        <AdminCard key={index} data={item} orderNumber={calculateOrderNumber(page,size,index)} />
+                        <AdminCard
+                            key={index}
+                            data={item}
+                            orderNumber={calculateOrderNumber(
+                                page,
+                                size,
+                                index
+                            )}
+                        />
                     ))
                 ) : (
                     <NoDataAvailable />
                 )}
             </div>
             <hr className={styles.hr} />
-            <Pagination />
+            <Pagination
+                baseUrl="/dashboard/manage/admin"
+                currentPage={+page}
+                size={size}
+                totalPages={data.totalPages}
+            />
         </div>
     );
 };
